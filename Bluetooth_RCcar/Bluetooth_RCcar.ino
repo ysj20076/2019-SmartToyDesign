3c8e5e30a615fc9c7be03707a795e22adf889448
Soo-Jeong <ysj20076@naver.com>
2019-05-09 23:13:46 +0900

    upload code

---
+++ Bluetooth_RCcar/Bluetooth_RCcar.ino
@@ -0,0 +1,55 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(4, 5);
+
+int in1 = 9;
+int in2 = 6;
+int in3 = 10;
+int in4 = 11;
+
+int RSpeed = 255;
+int LSpeed = 255;
+int a = 180;
+
+char data;
+
+void setup() {
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  if (BTSerial.available()) {
+    data = BTSerial.read();
+
+    if (data == 'f') { //직진
+      analogWrite(in1, 0);
+      analogWrite(in2, RSpeed);
+      analogWrite(in3, 0);
+      analogWrite(in4, LSpeed); 
+    }
+    if (data == 'r') { //우회전
+      analogWrite(in1, 0);
+      analogWrite(in2, a);
+      analogWrite(in3, 0);
+      analogWrite(in4, RSpeed);
+    }
+    if (data == 'l') { //좌회전
+      analogWrite(in1, 0);
+      analogWrite(in2, LSpeed);
+      analogWrite(in3, 0);
+      analogWrite(in4, a);
+    }
+    if (data == 'b') { //후진
+      analogWrite(in1, LSpeed);
+      analogWrite(in2, 0);
+      analogWrite(in3, RSpeed);
+      analogWrite(in4, 0);
+    }
+    if (data == 's') { //정지
+      analogWrite(in1, 0);
+      analogWrite(in2, 0);
+      analogWrite(in3, 0);
+      analogWrite(in4, 0);
+    }
+  }
+}
---
+++ Ultrasonic_RC/Ultrasonic_RC.ino
@@ -0,0 +1,63 @@
+int in1 = 9;
+int in2 = 6;
+int in3 = 10;
+int in4 = 11;
+
+int trig = 2;
+int echo = 3;
+
+int LSpeed = 255;
+int RSpeed = 255;
+int a = 185;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void loop() {
+  digitalWrite(trig, LOW);
+  digitalWrite(echo, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  unsigned long duration =
+    pulseIn(echo, HIGH);
+  float distance = duration / 29.0 / 2.0;
+
+  Serial.println(distance);
+
+  if (distance < 8) {
+    //정지
+    analogWrite(in1, 0);
+    analogWrite(in2, 0);
+    analogWrite(in3, 0);
+    analogWrite(in4, 0);
+    delay(700);
+
+    //후진
+    analogWrite(in1, LSpeed);
+      analogWrite(in2, 0);
+      analogWrite(in3, RSpeed);
+      analogWrite(in4, 0);
+      delay(500);
+
+    //좌회전
+    analogWrite(in1, 0);
+      analogWrite(in2, LSpeed);
+      analogWrite(in3, 0);
+      analogWrite(in4, a);
+    delay(500);
+  }
+
+  else { 
+    // 직진
+    analogWrite(in1, 0);
+      analogWrite(in2, RSpeed);
+      analogWrite(in3, 0);
+      analogWrite(in4, LSpeed);
+  }
+}
